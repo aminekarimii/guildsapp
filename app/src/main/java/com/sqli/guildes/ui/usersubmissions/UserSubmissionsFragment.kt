c09feb570b724e45005e07e3c61cb1b3594eac2d
Amine karimi <aminekarimi1998@gmail.com>
2020-03-25 18:28:34 +0100

    init user contributions files

---
+++ app/src/main/java/com/sqli/guildes/ui/usersubmissions/UserSubmissionsFragment.kt
@@ -0,0 +1,2 @@
+package com.sqli.guildes.ui.usersubmissions
+
---
+++ app/src/main/java/com/sqli/guildes/ui/usersubmissions/UserSubmissionsViewModel.kt
@@ -0,0 +1,6 @@
+package com.sqli.guildes.ui.usersubmissions
+
+import com.sqli.guildes.data.DataManager
+import com.sqli.guildes.ui.base.BaseViewModel
+
+class UserSubmissionsViewModel(dataManager: DataManager) : BaseViewModel(dataManager)
