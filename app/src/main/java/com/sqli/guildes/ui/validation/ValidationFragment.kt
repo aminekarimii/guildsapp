8f5b8b0bce45a158924665023f0c8424a1fac3c6
Amine karimi <aminekarimi1998@gmail.com>
2020-06-11 19:51:17 +0100

    Add confirmation dialog

--- app/src/main/java/com/sqli/guildes/ui/validation/ValidationFragment.kt
+++ app/src/main/java/com/sqli/guildes/ui/validation/ValidationFragment.kt
@@ -1,9 +1,11 @@
 package com.sqli.guildes.ui.validation
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import com.google.android.material.snackbar.Snackbar
@@ -13,12 +15,13 @@ import com.sqli.guildes.core.extensions.obtainViewModel
 import com.sqli.guildes.data.models.Submission
 import kotlinx.android.synthetic.main.fragment_validation.*
 
+
 class ValidationFragment : Fragment() {
 
     private lateinit var validationViewModel: ValidationViewModel
     private lateinit var contribId: String
 
-            override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
         return inflater.inflate(R.layout.fragment_validation, container, false)
     }
@@ -28,7 +31,7 @@ class ValidationFragment : Fragment() {
 
         validationViewModel = obtainViewModel(ValidationViewModel::class.java).apply {
             contribution.observe(this@ValidationFragment, Observer {
-                when(it){
+                when (it) {
                     is Resource.Success -> setView(it.data)
                 }
             })
@@ -39,10 +42,15 @@ class ValidationFragment : Fragment() {
             })
         }
         loadData()
-        btnValidate.setOnClickListener { validate() }
+        btnValidate.setOnClickListener {
+            showConfirmationDialog(
+                    R.string.validate_msg,
+                    R.string.validation_confirmation,
+                    context!!)
+        }
     }
 
-    private fun setView(data:Submission){
+    private fun setView(data: Submission) {
         tvContrib.text = "${data.createdBy.lastname} ${data.createdBy.firstname}"
         tvProfileGuildePoints.text = data.points.toString()
         tvContribTitle.text = data.subject
@@ -58,12 +66,28 @@ class ValidationFragment : Fragment() {
 
     }
 
-    private fun validate(){
+    private fun validate() {
         validationViewModel.validateSubmission(contribId)
     }
 
     private fun showSnackbar(viewRes: Int, message: Int, length: Int = Snackbar.LENGTH_LONG) {
         Snackbar.make(activity!!.findViewById(viewRes), message, length).show()
+    }
+
+    private fun showConfirmationDialog(message: Int, title: Int, context: Context) {
+        val alertDialog: AlertDialog? = activity?.let {
+            AlertDialog.Builder(it).apply {
+                setPositiveButton(R.string.validate) { dialog, id ->
+                    validate()
+                }
+                setNegativeButton(R.string.cancel) { dialog, id ->
+                    dialog.dismiss()
+                }
+                setMessage(message)?.setTitle(title)
+            }.create()
+        }
+
+        alertDialog?.show()
     }
 
 }
